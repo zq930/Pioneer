36e87d899f9a0ca415d5c9a74bffc777efedb30c
huangliming <aihlm@qq.com>
2022-04-09 17:39:22 +0800

    去除fastjson，使用jackson

--- src/main/java/com/pioneer/framework/config/RedisConfig.java
+++ src/main/java/com/pioneer/framework/config/RedisConfig.java
@@ -2,7 +2,6 @@ package com.pioneer.framework.config;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
@@ -30,28 +29,32 @@ public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         ObjectMapper objectMapper = new ObjectMapper();
-        //反序列化时候遇到不匹配的属性并不抛出异常
+        // 反序列化时候遇到不匹配的属性并不抛出异常
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        //序列化时候遇到空对象不抛出异常
+        // 序列化时候遇到空对象不抛出异常
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        //反序列化的时候如果是无效子类型,不抛出异常
+        // 反序列化的时候如果是无效子类型,不抛出异常
         objectMapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
-        //不使用默认的dateTime进行序列化,
+        // 不使用默认的dateTime进行序列化,
         objectMapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
-        //使用JSR310提供的序列化类,里面包含了大量的JDK8时间序列化类
+        // 使用JSR310提供的序列化类,里面包含了大量的JDK8时间序列化类
         objectMapper.registerModule(new JavaTimeModule());
-        //启用反序列化所需的类型信息,在属性中添加@class
+        // 启用反序列化所需的类型信息,在属性中添加@class
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-        //配置null值的序列化器
+        // 配置null值的序列化器
         GenericJackson2JsonRedisSerializer.registerNullValueSerializer(objectMapper, null);
 
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        // Redis模板设置
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        // 默认序列化方式
         template.setDefaultSerializer(serializer);
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(StringRedisSerializer.UTF_8);
+        template.setValueSerializer(serializer);
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashValueSerializer(serializer);
         template.setHashKeySerializer(StringRedisSerializer.UTF_8);
         template.afterPropertiesSet();
         return template;
